3d8e3cab4a979c74e0083aabbb13b2d196ea93a0
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-12-24 12:57:40 +0100

    Add blacklist-based validation logic in ModerationService

--- src/main/java/com/moderation/moderation_service/controller/ModerationController.java
+++ src/main/java/com/moderation/moderation_service/controller/ModerationController.java
@@ -6,14 +6,22 @@ import org.springframework.web.bind.annotation.*;
 import com.moderation.moderation_service.dto.ModerationRequest;
 import com.moderation.moderation_service.dto.ModerationResponse;
 
+import com.moderation.moderation_service.service.ModerationService;
+
 @RestController
 @RequestMapping("/moderation")
 
 public class ModerationController {
 
+  private final ModerationService moderationService;
+
+  public ModerationController(ModerationService moderationService) {
+    this.moderationService = moderationService;
+  }
+
   @PostMapping("/validate-message")
   public ResponseEntity<ModerationResponse> validateMessage(@RequestBody ModerationRequest request) {
-    boolean isValid = !request.getMessage().toLowerCase().contains("spam");
+    boolean isValid = moderationService.validateMessage(request.getMessage());
     return ResponseEntity.ok(new ModerationResponse(isValid));
   }
 }
